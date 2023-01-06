e73f86dce4297a1715f41ad3f2d083e1d4f0d141
making-a-scene <4936joo@naver.com>
2023-01-06 19:32:33 +0900

    User 엔티티 필드에 unique, nullable 속성 추가

--- src/main/java/buildup/buildupback/User.java
+++ src/main/java/buildup/buildupback/User.java
@@ -1,2 +1,36 @@
-package buildup.buildupback;public class User {
+package buildup.buildupback;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+@Getter
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String nickname;
+    @Column(nullable = false, unique = true)
+    private String phone;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private Date birth;
+
+    private Provider social;
+    private String refreshToken;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
