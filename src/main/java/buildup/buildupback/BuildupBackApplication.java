763dcb9d1e9c1523e256cfd57bfcfb0c1002ab4e
making-a-scene <4936joo@naver.com>
2023-01-06 19:16:40 +0900

    프로젝트 초기 설정

---
+++ src/main/java/buildup/buildupback/BuildupBackApplication.java
@@ -0,0 +1,13 @@
+package buildup.buildupback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildupBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildupBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/buildup/buildupback/BuildupBackApplicationTests.java
@@ -0,0 +1,13 @@
+package buildup.buildupback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildupBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
