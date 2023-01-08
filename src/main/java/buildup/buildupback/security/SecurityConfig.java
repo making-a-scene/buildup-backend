139b4f92e0bc8746edff64ae9556816e7cfd274c
making-a-scene <4936joo@naver.com>
2023-01-08 14:17:14 +0900

    SecurityConfig 대략적인 설정

--- src/main/java/buildup/buildupback/security/SecurityConfig.java
+++ src/main/java/buildup/buildupback/security/SecurityConfig.java
@@ -1,2 +1,50 @@
-package buildup.buildupback.security;public class SecurityConfig {
+package buildup.buildupback.security;
+
+import buildup.buildupback.security.local.LocalUserDetailsService;
+import buildup.buildupback.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+    private final UserService userService;
+    private final LocalUserDetailsService localUserDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(localUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .httpBasic().disable();
+        http.authorizeHttpRequests()
+                .requestMatchers("/admin/**").hasRole("ADMIN");
+        return http.build();
+    }
 }
